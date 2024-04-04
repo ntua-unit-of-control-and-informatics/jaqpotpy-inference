d54bad2916734d16bbce5732d9dec70c31e0ea78
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-04-04 11:13:20 +0300

    fix: run/debug uvicorn in local

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from src.handlers.predict import model_post_handler
 from src.entities.prediction_request import PredictionRequestPydantic
@@ -13,3 +14,6 @@ def hello_world():
 @app.post('/predict/')
 def predict(req: PredictionRequestPydantic):
     return JSONResponse(content = model_post_handler(req))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)
