33e0563c6ac9f923caf18333720d78281fb66210
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-05-31 19:35:38 +0300

    feat/Add decode_with_external
    This commit introduces a new function. decode_with_external() is similar to decode() and is used in the case that the model receives as input external (user defined) data along with smiles.

--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -2,3 +2,13 @@ def decode(request):
     dataset = request.dataset
     data_entry_all = [item['values']['0'] for item in dataset['dataEntry']]
     return data_entry_all
+
+def decode_with_external(request):
+    dataset = request.dataset
+    sorted_features = sorted(request.dataset['features'], key=lambda x: int(x['key']))
+    names_sorted_by_key = [item['name'] for item in sorted_features]
+    input_values = list(dataset['dataEntry'][0]['values'].values())
+    input_values = [[i] for i in input_values]
+    data_entry_all = dict(zip(names_sorted_by_key, input_values))
+    Smiles_input = [data_entry_all.pop('Smiles')][0]
+    return Smiles_input, data_entry_all
