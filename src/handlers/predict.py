468dc67a01150c684ab8929b7ef7128598d68272
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-07-22 13:31:57 +0300

    feat(JAQPOT-186): Update jaqpotpy-inference (#9)
    * feat: Update jaqpotpy-inference
    
    With this commit we can take predictions from models of SklearnModel class.
    
    * fix: declare imports
    
    * feat: support models with multiple output variables
    
    * chore: simplify the code of model_post_handler

--- src/handlers/predict.py
+++ src/handlers/predict.py
@@ -1,31 +1,26 @@
 from ..entities.prediction_request import PredictionRequestPydantic
 from ..helpers import model_decoder, json_to_predreq
 
-
 def model_post_handler(request: PredictionRequestPydantic):
     model = model_decoder.decode(request.model['rawModel'])
-    data_entry_all = json_to_predreq.decode(request)
-    _ = model(data_entry_all)
+    data_entry_all = json_to_predreq.decode(request, model)
+    prediction = model.predict_onnx(data_entry_all)
+    if model.task == 'classification':
+        probabilities = model.predict_proba_onnx(data_entry_all)
+    else:
+        probabilities = [None for _ in range(len(prediction))]
 
-    if isinstance(model.prediction[0], list):
-        results = {model.Y[i]: [item[i] for item in model.prediction] for i in range(len(model.prediction[0]))}
-    elif isinstance(model.prediction, list):
-        if isinstance(model.Y, list):
-            results = {model.Y[0]: [item for item in model.prediction]}
+    results = {}
+    for i, feature in enumerate(model.dependentFeatures):
+        key = feature['key']
+        if len(model.dependentFeatures) == 1:
+            values = [str(item) for item in prediction]
         else:
-            results = {model.Y: [item for item in model.prediction]}
-    else:
-        results = {model.Y: [item for item in model.prediction]}
+            values = [str(item) for item in prediction[:, i]]
+        results[key] = values
 
-    if model.doa:
-        results['AD'] = model.doa.IN
-    else:
-        results['AD'] = [None for _ in range(len(model.prediction))]
-
-    if model.probability:
-        results['Probabilities'] = [list(prob) for prob in model.probability]
-    else:
-        results['Probabilities'] = [[] for _ in range(len(model.prediction))]
+    results['Probabilities'] = [str(prob) for prob in probabilities]
+    results['AD'] = [None for _ in range(len(prediction))]
 
     final_all = {"predictions": [dict(zip(results, t)) for t in zip(*results.values())]}
 
--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -1,9 +1,11 @@
-def decode(request):
-    dataset = request.dataset
-    model = request.model
+from jaqpotpy.datasets import JaqpotpyDataset
+import pandas as pd
 
-    keys = [feature['key'] for feature in model['independentFeatures']]
-    transformed_values = [[data[key] for key in keys] for data in dataset['input']]
-
-    # TODO fix to support multiple rows
-    return transformed_values[0]
+def decode(request, model):
+    df = pd.DataFrame(request.dataset['input'])
+    independentFeatures = request.model['independentFeatures']
+    smiles_cols = [feature['key'] for feature in independentFeatures if feature['featureType'] == 'SMILES']
+    x_cols = [feature['key'] for feature in independentFeatures if feature['featureType'] != 'SMILES']
+    dataset = JaqpotpyDataset(df=df, smiles_cols=smiles_cols,  x_cols=x_cols,
+                              task=model.task, featurizer=model.featurizer)
+    return dataset
