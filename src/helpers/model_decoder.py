c2abdf58e9fdfcaa10b5f5daafc9cc923b272993
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-07-24 14:27:26 +0300

    fix: change the depickling process

--- src/helpers/model_decoder.py
+++ src/helpers/model_decoder.py
@@ -1,8 +1,11 @@
-from base64 import b64encode, b64decode
+from base64 import b64decode
 import pickle
-
+import compress_pickle
 
 def decode(rawModel):
     model = b64decode(rawModel)
-    p_mod = pickle.loads(model)
+    try:
+        p_mod = compress_pickle.loads(model, compression='bz2')
+    except Exception as e:
+        p_mod = pickle.loads(model)
     return p_mod
