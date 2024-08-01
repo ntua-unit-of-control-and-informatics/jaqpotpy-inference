dabb25b23ca8cc52a58b3b85e8581af6d20195e6
Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>
2024-08-01 14:08:51 +0300

    feat(JAQPOT-161): json logging for grafana (#13)
    * feat(JAQPOT-161): json logging for grafana
    
    * fix: flake8 issues
    
    * fix: log exception when depickling

--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -1,11 +1,12 @@
 from jaqpotpy.datasets import JaqpotpyDataset
 import pandas as pd
 
+
 def decode(request, model):
     df = pd.DataFrame(request.dataset['input'])
-    independentFeatures = request.model['independentFeatures']
-    smiles_cols = [feature['key'] for feature in independentFeatures if feature['featureType'] == 'SMILES']
-    x_cols = [feature['key'] for feature in independentFeatures if feature['featureType'] != 'SMILES']
+    independent_features = request.model['independentFeatures']
+    smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES']
+    x_cols = [feature['key'] for feature in independent_features if feature['featureType'] != 'SMILES']
     dataset = JaqpotpyDataset(df=df, smiles_cols=smiles_cols,  x_cols=x_cols,
                               task=model.task, featurizer=model.featurizer)
     return dataset
