9f494cbeca3b1b33231f60e85b6f4972b1c9abcc
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-08-01 14:04:29 +0300

    fix: log exception when depickling

--- src/helpers/model_decoder.py
+++ src/helpers/model_decoder.py
@@ -1,6 +1,7 @@
 from base64 import b64decode
 import pickle
 import compress_pickle
+from src.loggers.logger import logger
 
 
 def decode(raw_model):
@@ -8,5 +9,6 @@ def decode(raw_model):
     try:
         p_mod = compress_pickle.loads(model, compression='bz2')
     except Exception as e:
+        logger.warning('Error at %s', 'Could not depickle model', exc_info=e)
         p_mod = pickle.loads(model)
     return p_mod
