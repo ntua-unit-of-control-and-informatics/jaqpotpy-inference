096dbbb44c70e780d696bc7f517f9ef26e3115fc
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-08-01 13:49:45 +0300

    feat(JAQPOT-161): json logging for grafana

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from fastapi import FastAPI
 from src.handlers.predict import model_post_handler
 from src.entities.prediction_request import PredictionRequestPydantic
 from fastapi.responses import JSONResponse
+from src.loggers.log_middleware import LogMiddleware
 
 
 app = FastAPI()
+app.add_middleware(LogMiddleware)
+
 
 @app.get('/')
 def health_check():
     return {'status': 'UP'}
 
+
 @app.post('/predict/')
 def predict(req: PredictionRequestPydantic):
-    return JSONResponse(content = model_post_handler(req))
+    return JSONResponse(content=model_post_handler(req))
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run(app, host="0.0.0.0", port=8002, log_config=None)
