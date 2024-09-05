f703a797a8c295288783502639d8d288ab3a20eb
johnsaveus <ioannis.savvas00@gmail.com>
2024-09-05 11:28:01 +0300

    remove predict_graph

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from src.handlers.predict_graph import  graph_post_handler
+from src.handlers.predict import  model_post_handler, graph_post_handler
 from src.entities.prediction_request import PredictionRequestPydantic
 from fastapi.responses import JSONResponse
 
@@ -12,8 +12,12 @@ def health_check():
 
 @app.post('/predict/')
 def predict(req: PredictionRequestPydantic):
-    #return JSONResponse(content = model_post_handler(req))
-    return JSONResponse(content = graph_post_handler(req))
+    if req.model['type'] == 'SKLEARN':
+        return JSONResponse(content = model_post_handler(req))
+    elif req.model['type'] == 'TORCH':
+        return JSONResponse(content = graph_post_handler(req))
+    else:
+        raise ValueError('Only SKLEARN and TORCH models are supported')
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
