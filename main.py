bccc3b6e0d55b28b1e7f411373a1ea6adaa31437
johnsaveus <ioannis.savvas00@gmail.com>
2024-09-05 16:58:11 +0300

    ruff_format

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import uvicorn
 from fastapi import FastAPI
-from src.handlers.predict import  model_post_handler, graph_post_handler
+from src.handlers.predict import model_post_handler, graph_post_handler
 from src.entities.prediction_request import PredictionRequestPydantic
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get('/')
-def health_check():
-    return {'status': 'UP'}
 
-@app.post('/predict/')
+@app.get("/")
+def health_check():
+    return {"status": "UP"}
+
+
+@app.post("/predict/")
 def predict(req: PredictionRequestPydantic):
-    if req.model['type'] == 'SKLEARN':
-        return JSONResponse(content = model_post_handler(req))
-    elif req.model['type'] == 'TORCH':
-        return JSONResponse(content = graph_post_handler(req))
+    if req.model["type"] == "SKLEARN":
+        return JSONResponse(content=model_post_handler(req))
+    elif req.model["type"] == "TORCH":
+        return JSONResponse(content=graph_post_handler(req))
     else:
-        raise ValueError('Only SKLEARN and TORCH models are supported')
+        raise ValueError("Only SKLEARN and TORCH models are supported")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
--- src/handlers/predict.py
+++ src/handlers/predict.py
@@ -7,71 +7,85 @@ import torch
 import torch.nn.functional as F
 import sys
 import os
+
 # current_dir = os.path.dirname(__file__)
 # software_dir = os.path.abspath(os.path.join(current_dir, '../../../../../JQP'))
 # sys.path.append(software_dir)
 from jaqpotpy.descriptors.graph.graph_featurizer import SmilesGraphFeaturizer
 import torch.nn.functional as F
 
+
 def model_post_handler(request: PredictionRequestPydantic):
-    model = model_decoder.decode(request.model['rawModel'])
+    model = model_decoder.decode(request.model["rawModel"])
     data_entry_all = json_to_predreq.decode(request, model)
     prediction = model.predict_onnx(data_entry_all)
-    if model.task == 'classification':
+    if model.task == "classification":
         probabilities = model.predict_proba_onnx(data_entry_all)
     else:
         probabilities = [None for _ in range(len(prediction))]
 
     results = {}
     for i, feature in enumerate(model.dependentFeatures):
-        key = feature['key']
+        key = feature["key"]
         if len(model.dependentFeatures) == 1:
             values = [str(item) for item in prediction]
         else:
             values = [str(item) for item in prediction[:, i]]
         results[key] = values
 
-    results['Probabilities'] = [str(prob) for prob in probabilities]
-    results['AD'] = [None for _ in range(len(prediction))]
+    results["Probabilities"] = [str(prob) for prob in probabilities]
+    results["AD"] = [None for _ in range(len(prediction))]
 
     final_all = {"predictions": [dict(zip(results, t)) for t in zip(*results.values())]}
 
     return final_all
 
+
 def graph_post_handler(request: PredictionRequestPydantic):
     # Obtain the request info
-    onnx_model = base64.b64decode(request.model['rawModel'])
+    onnx_model = base64.b64decode(request.model["rawModel"])
     ort_session = onnxruntime.InferenceSession(onnx_model)
-    feat_config = request.extraConfig['torchConfig']['featurizer']   
+    feat_config = request.extraConfig["torchConfig"]["featurizer"]
     # Load the featurizer
     featurizer = SmilesGraphFeaturizer()
     featurizer.load_json_rep(feat_config)
-    smiles = request.dataset['input'][0]['SMILES']
+    smiles = request.dataset["input"][0]["SMILES"]
+
     def to_numpy(tensor):
-         return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+        return (
+            tensor.detach().cpu().numpy()
+            if tensor.requires_grad
+            else tensor.cpu().numpy()
+        )
 
     data = featurizer.featurize(smiles)
     # ONNX Inference
-    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(data.x),
-              ort_session.get_inputs()[1].name: to_numpy(data.edge_index),
-              ort_session.get_inputs()[2].name: to_numpy(torch.zeros(data.x.shape[0], dtype=torch.int64))}
+    ort_inputs = {
+        ort_session.get_inputs()[0].name: to_numpy(data.x),
+        ort_session.get_inputs()[1].name: to_numpy(data.edge_index),
+        ort_session.get_inputs()[2].name: to_numpy(
+            torch.zeros(data.x.shape[0], dtype=torch.int64)
+        ),
+    }
     ort_outs = torch.tensor(np.array(ort_session.run(None, ort_inputs)))
-    if request.extraConfig['torchConfig']['task'] == 'classification':
+    if request.extraConfig["torchConfig"]["task"] == "classification":
         return graph_binary_classification(request, ort_outs)
-    
+
+
 def graph_binary_classification(request: PredictionRequestPydantic, onnx_output):
     # Classification
-    target_name = request.model['dependentFeatures'][0]['name']
+    target_name = request.model["dependentFeatures"][0]["name"]
     probs = [F.sigmoid(onnx_output).squeeze().tolist()]
     preds = [int(prob > 0.5) for prob in probs]
     # UI Results
     results = {}
-    results['Probabilities'] = [str(prob) for prob in probs]
+    results["Probabilities"] = [str(prob) for prob in probs]
     results[target_name] = [str(pred) for pred in preds]
     final_all = {"predictions": [dict(zip(results, t)) for t in zip(*results.values())]}
     print(final_all)
 
     return final_all
+
 
 # def graph_regression(request: PredictionRequestPydantic, onnx_output):
 #     # Regression
