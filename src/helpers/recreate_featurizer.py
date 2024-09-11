3dc8fa3c70319f9355d50dd309f6ed6db996b66d
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-11 21:59:41 +0300

    feat:add recreate_featurizer method

---
+++ src/helpers/recreate_featurizer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def recreate_featurizer(featurizer_name,  featurizer_config):
+    featurizer_class = getattr(__import__(
+        'jaqpotpy.descriptors.molecular',
+          fromlist=[featurizer_name]), featurizer_name)
+    featurizer = featurizer_class()
+    for attr, value in featurizer_config.items():
+        if attr !='class': # skip the class attribute
+            if isinstance(value, list):
+                value = np.array(value['value'])
+            setattr(featurizer, attr, value['value'])
+    return featurizer
