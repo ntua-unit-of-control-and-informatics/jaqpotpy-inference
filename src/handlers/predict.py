7d66b8120a24db0df1378e00863fb0527be19936
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-11 22:13:34 +0300

    fix:predict methods

--- src/handlers/predict.py
+++ src/handlers/predict.py
@@ -1,5 +1,6 @@
 from ..entities.prediction_request import PredictionRequestPydantic
-from ..helpers import model_decoder, json_to_predreq, 
+from ..helpers import model_decoder, json_to_predreq
+from ..helpers.predict_methods import predict_onnx
 import base64
 import onnxruntime
 import numpy as np
@@ -18,17 +19,16 @@ from jaqpotpy.descriptors.graph.graph_featurizer import SmilesGraphFeaturizer
 def model_post_handler(request: PredictionRequestPydantic):
     model = model_decoder.decode(request.model["rawModel"])
     data_entry_all = json_to_predreq.decode(request)
-    # prediction = model.predict_onnx(data_entry_all)
-    prediction = 
-    if model.task == "classification":
-        probabilities = model.predict_proba_onnx(data_entry_all)
-    else:
-        probabilities = [None for _ in range(len(prediction))]
+    prediction = predict_onnx(model, data_entry_all, request)
+    # if model.task == "classification":
+    #     probabilities = model.predict_proba_onnx(data_entry_all)
+    # else:
+    probabilities = [None for _ in range(len(prediction))]
 
     results = {}
-    for i, feature in enumerate(model.dependentFeatures):
+    for i, feature in enumerate(request.model['dependentFeatures']):
         key = feature['key']
-        if len(model.dependentFeatures) == 1:
+        if len(request.model['dependentFeatures']) == 1:
             values = [str(item) for item in prediction]
         else:
             values = [str(item) for item in prediction[:, i]]
--- src/helpers/predict_methods.py
+++ src/helpers/predict_methods.py
@@ -9,10 +9,4 @@ def predict_onnx(model, dataset: JaqpotpyDataset, request):
     )
     if len(request.model['dependentFeatures']) == 1:
         onnx_prediction[0] = onnx_prediction[0].reshape(-1, 1)
-    if self.preprocess is not None:
-        if self.preprocessing_y:
-            for f in self.preprocessing_y:
-                onnx_prediction[0] = f.inverse_transform(onnx_prediction[0])
-    if len(self.y_cols) == 1:
-        return onnx_prediction[0].flatten()
     return onnx_prediction[0]
