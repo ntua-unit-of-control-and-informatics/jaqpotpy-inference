60233ab2bb92a2814fe685e6f184399943257cf5
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-11 21:42:27 +0300

    migrate changes

--- src/handlers/predict.py
+++ src/handlers/predict.py
@@ -1,12 +1,26 @@
 from ..entities.prediction_request import PredictionRequestPydantic
-from ..helpers import model_decoder, json_to_predreq
+from ..helpers import model_decoder, json_to_predreq, 
+import base64
+import onnxruntime
+import numpy as np
+import torch
+import torch.nn.functional as F
+from jaqpotpy.descriptors.graph.graph_featurizer import SmilesGraphFeaturizer
+
+# import sys
+# import os
+
+# current_dir = os.path.dirname(__file__)
+# software_dir = os.path.abspath(os.path.join(current_dir, '../../../../../JQP'))
+# sys.path.append(software_dir)
 
 
 def model_post_handler(request: PredictionRequestPydantic):
-    model = model_decoder.decode(request.model['rawModel'])
-    data_entry_all = json_to_predreq.decode(request, model)
-    prediction = model.predict_onnx(data_entry_all)
-    if model.task == 'classification':
+    model = model_decoder.decode(request.model["rawModel"])
+    data_entry_all = json_to_predreq.decode(request)
+    # prediction = model.predict_onnx(data_entry_all)
+    prediction = 
+    if model.task == "classification":
         probabilities = model.predict_proba_onnx(data_entry_all)
     else:
         probabilities = [None for _ in range(len(prediction))]
--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -1,12 +1,18 @@
 from jaqpotpy.datasets import JaqpotpyDataset
 import pandas as pd
+from src.helpers.recreate_featurizer import recreate_featurizer
 
-
-def decode(request, model):
+def decode(request):
     df = pd.DataFrame(request.dataset['input'])
     independent_features = request.model['independentFeatures']
     smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES']
     x_cols = [feature['key'] for feature in independent_features if feature['featureType'] != 'SMILES']
+
+    if request.model['extraConfig']['featurizers']:
+        featurizer_name = request.model['extraConfig']['featurizers'][0]['name']
+        featurizer_config = request.model['extraConfig']['featurizers'][0]['config']
+        featurizer = recreate_featurizer(featurizer_name, featurizer_config)
+
     dataset = JaqpotpyDataset(df=df, smiles_cols=smiles_cols,  x_cols=x_cols,
-                              task=model.task, featurizer=model.featurizer)
+                              task=request.model['task'].lower(), featurizer=featurizer)
     return dataset
