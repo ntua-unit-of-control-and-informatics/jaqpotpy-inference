6071042db09e23a7baee39630daff8f093e3ffda
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-12 12:31:36 +0300

    fix: no featurizer-models supported

--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -6,13 +6,16 @@ from jaqpotpy.datasets import JaqpotpyDataset
 def decode(request):
     df = pd.DataFrame(request.dataset['input'])
     independent_features = request.model['independentFeatures']
-    smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES']
+    # smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES']
+    smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES'] or None
     x_cols = [feature['key'] for feature in independent_features if feature['featureType'] != 'SMILES']
 
     if request.model['extraConfig']['featurizers']:
         featurizer_name = request.model['extraConfig']['featurizers'][0]['name']
         featurizer_config = request.model['extraConfig']['featurizers'][0]['config']
         featurizer = recreate_featurizer(featurizer_name, featurizer_config)
+    else:
+        featurizer = None
 
     dataset = JaqpotpyDataset(df=df, smiles_cols=smiles_cols,  x_cols=x_cols,
                               task=request.model['task'].lower(), featurizer=featurizer)
