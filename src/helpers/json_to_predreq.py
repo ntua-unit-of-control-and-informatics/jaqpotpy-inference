8062730ec6be397917cb503a0c8fab5209409eba
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-17 11:35:33 +0300

    fix: support multiple featurizers
    Added a for loop to recreate all requested featurizers. However, JaqpotpyDatast is not ready for getting a list of multiple featurizers. When we will add this feature in JqpotpyDataset, featurizers[0] will change to featurizers

--- src/helpers/json_to_predreq.py
+++ src/helpers/json_to_predreq.py
@@ -4,19 +4,34 @@ from jaqpotpy.datasets import JaqpotpyDataset
 
 
 def decode(request):
-    df = pd.DataFrame(request.dataset['input'])
-    independent_features = request.model['independentFeatures']
+    df = pd.DataFrame(request.dataset["input"])
+    independent_features = request.model["independentFeatures"]
     # smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES']
-    smiles_cols = [feature['key'] for feature in independent_features if feature['featureType'] == 'SMILES'] or None
-    x_cols = [feature['key'] for feature in independent_features if feature['featureType'] != 'SMILES']
-
-    if request.model['extraConfig']['featurizers']:
-        featurizer_name = request.model['extraConfig']['featurizers'][0]['name']
-        featurizer_config = request.model['extraConfig']['featurizers'][0]['config']
-        featurizer = recreate_featurizer(featurizer_name, featurizer_config)
+    smiles_cols = [
+        feature["key"]
+        for feature in independent_features
+        if feature["featureType"] == "SMILES"
+    ] or None
+    x_cols = [
+        feature["key"]
+        for feature in independent_features
+        if feature["featureType"] != "SMILES"
+    ]
+    featurizers = []
+    if request.model["extraConfig"]["featurizers"]:
+        for i in range(len(request.model["extraConfig"]["featurizers"])):
+            featurizer_name = request.model["extraConfig"]["featurizers"][i]["name"]
+            featurizer_config = request.model["extraConfig"]["featurizers"][i]["config"]
+            featurizer = recreate_featurizer(featurizer_name, featurizer_config)
+            featurizers.append(featurizer)
     else:
-        featurizer = None
+        featurizers = None
 
-    dataset = JaqpotpyDataset(df=df, smiles_cols=smiles_cols,  x_cols=x_cols,
-                              task=request.model['task'].lower(), featurizer=featurizer)
+    dataset = JaqpotpyDataset(
+        df=df,
+        smiles_cols=smiles_cols,
+        x_cols=x_cols,
+        task=request.model["task"].lower(),
+        featurizer=featurizers[0] if isinstance(featurizers, list) else featurizers,
+    )
     return dataset
