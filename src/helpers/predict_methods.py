e0b5e46186d1ed96e46625d2514719c5d2dc2686
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-09-27 16:49:40 +0300

    refactor(JAQPOT-324): sklearn predict methods (#21)
    * refactor: predict methods for sklearn
    
    ...according to the refactoring of the onnx set in jaqpotpy
    
    * fix: simplify the np_dtype checks
    
    * chore: remove conversion of dtypes

--- src/helpers/predict_methods.py
+++ src/helpers/predict_methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+import onnx
 from onnxruntime import InferenceSession
 from jaqpotpy.datasets import JaqpotpyDataset
 from src.helpers.recreate_preprocessor import recreate_preprocessor
@@ -6,14 +7,27 @@ from src.helpers.recreate_preprocessor import recreate_preprocessor
 
 def predict_onnx(model, dataset: JaqpotpyDataset, request):
     sess = InferenceSession(model.SerializeToString())
-    onnx_prediction = sess.run(
-        None, {"float_input": dataset.X.to_numpy().astype(np.float32)}
-    )
+    input_feed = {}
+    for independent_feature in model.graph.input:
+        np_dtype = onnx.helper.tensor_dtype_to_np_dtype(
+            independent_feature.type.tensor_type.elem_type
+        )
+        # if np_dtype == "float64":
+        #     np_dtype = "float32"
+        # elif np_dtype == ["int64", "uint64"]:
+        #     np_dtype = "int32"
+
+        if len(model.graph.input) == 1:
+            input_feed[independent_feature.name] = dataset.X.values.astype(np_dtype)
+        else:
+            input_feed[independent_feature.name] = (
+                dataset.X[independent_feature.name]
+                .values.astype(np_dtype)
+                .reshape(-1, 1)
+            )
+    onnx_prediction = sess.run(None, input_feed)
     if len(request.model["dependentFeatures"]) == 1:
         onnx_prediction = onnx_prediction[0].reshape(-1, 1)
-        # onnx_prediction is being reshaped to a 2D array to avoid errors
-        # when the model has only one dependent feature. In multi-output models,
-        # onnx_prediction is already a 2D array.
     else:
         onnx_prediction = onnx_prediction[0]
 
@@ -36,9 +50,20 @@ def predict_onnx(model, dataset: JaqpotpyDataset, request):
 
 def predict_proba_onnx(model, dataset: JaqpotpyDataset, request):
     sess = InferenceSession(model.SerializeToString())
-    onnx_probs = sess.run(
-        None, {"float_input": dataset.X.to_numpy().astype(np.float32)}
-    )
+    input_feed = {}
+    for independent_feature in model.graph.input:
+        np_dtype = onnx.helper.tensor_dtype_to_np_dtype(
+            independent_feature.type.tensor_type.elem_type
+        )
+        if len(model.graph.input) == 1:
+            input_feed[independent_feature.name] = dataset.X.values.astype(np_dtype)
+        else:
+            input_feed[independent_feature.name] = (
+                dataset.X[independent_feature.name]
+                .values.astype(np_dtype)
+                .reshape(-1, 1)
+            )
+    onnx_probs = sess.run(None, input_feed)
     onnx_probs_list = [
         max(onnx_probs[1][instance].values()) for instance in range(len(onnx_probs[1]))
     ]
