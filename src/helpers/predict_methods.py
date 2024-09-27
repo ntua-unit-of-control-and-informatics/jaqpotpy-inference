1d2bb3b6cce3d2f7a82c0c0da4906e6d750027bf
Vassilis Minadakis <vassilisminadakis@gmail.com>
2024-09-27 15:57:06 +0300

    refactor: predict methods for sklearn
    ...according to the refactoring of the onnx set in jaqpotpy

--- src/helpers/predict_methods.py
+++ src/helpers/predict_methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+import onnx
 from onnxruntime import InferenceSession
 from jaqpotpy.datasets import JaqpotpyDataset
 from src.helpers.recreate_preprocessor import recreate_preprocessor
@@ -6,9 +7,24 @@ from src.helpers.recreate_preprocessor import recreate_preprocessor
 
 def predict_onnx(model, dataset: JaqpotpyDataset, request):
     sess = InferenceSession(model.SerializeToString())
-    onnx_prediction = sess.run(
-        None, {"float_input": dataset.X.to_numpy().astype(np.float32)}
-    )
+    input_feed = {}
+    for independent_feature in model.graph.input:
+        np_dtype = onnx.helper.tensor_dtype_to_np_dtype(
+            independent_feature.type.tensor_type.elem_type
+        )
+        if np_dtype in ["float32", "float64", "int32", "int64"]:
+            target_dtype = np_dtype
+        else:
+            target_dtype = "object"
+        if len(model.graph.input) == 1:
+            input_feed[independent_feature.name] = dataset.X.values.astype(target_dtype)
+        else:
+            input_feed[independent_feature.name] = (
+                dataset.X[independent_feature.name]
+                .values.astype(target_dtype)
+                .reshape(-1, 1)
+            )
+    onnx_prediction = sess.run(None, input_feed)
     if len(request.model["dependentFeatures"]) == 1:
         onnx_prediction = onnx_prediction[0].reshape(-1, 1)
         # onnx_prediction is being reshaped to a 2D array to avoid errors
@@ -36,9 +52,24 @@ def predict_onnx(model, dataset: JaqpotpyDataset, request):
 
 def predict_proba_onnx(model, dataset: JaqpotpyDataset, request):
     sess = InferenceSession(model.SerializeToString())
-    onnx_probs = sess.run(
-        None, {"float_input": dataset.X.to_numpy().astype(np.float32)}
-    )
+    input_feed = {}
+    for independent_feature in model.graph.input:
+        np_dtype = onnx.helper.tensor_dtype_to_np_dtype(
+            independent_feature.type.tensor_type.elem_type
+        )
+        if np_dtype in ["float32", "float64", "int32", "int64"]:
+            target_dtype = np_dtype
+        else:
+            target_dtype = "object"
+        if len(model.graph.input) == 1:
+            input_feed[independent_feature.name] = dataset.X.values.astype(target_dtype)
+        else:
+            input_feed[independent_feature.name] = (
+                dataset.X[independent_feature.name]
+                .values.astype(target_dtype)
+                .reshape(-1, 1)
+            )
+    onnx_probs = sess.run(None, input_feed)
     onnx_probs_list = [
         max(onnx_probs[1][instance].values()) for instance in range(len(onnx_probs[1]))
     ]
