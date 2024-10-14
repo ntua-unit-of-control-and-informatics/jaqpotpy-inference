08b1666ac3bd5abdafa435837dec799773c769ff
Vassilis Minadakis <56068291+vassilismin@users.noreply.github.com>
2024-10-14 10:58:06 +0300

    feat: implement LabelEncoder (#29)

--- src/helpers/recreate_preprocessor.py
+++ src/helpers/recreate_preprocessor.py
@@ -1,13 +1,15 @@
 import numpy as np
 
-def recreate_preprocessor(preprocessor_name,  preprocessor_config):
-    preprocessor_class = getattr(__import__('sklearn.preprocessing', 
-                                            fromlist=[preprocessor_name]),
-                                              preprocessor_name)
+
+def recreate_preprocessor(preprocessor_name, preprocessor_config):
+    preprocessor_class = getattr(
+        __import__("sklearn.preprocessing", fromlist=[preprocessor_name]),
+        preprocessor_name,
+    )
     preprocessor = preprocessor_class()
     for attr, value in preprocessor_config.items():
-        if attr !='class': # skip the class attribute
+        if attr != "class":  # skip the class attribute
             if isinstance(value, list):
-                value = np.array(value['value'])
-            setattr(preprocessor, attr, value['value'])
+                value = np.array(value)
+            setattr(preprocessor, attr, value)
     return preprocessor
